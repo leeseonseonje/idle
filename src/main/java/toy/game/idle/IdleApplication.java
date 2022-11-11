9fb74d641ba9fade4231c4aa55a1cb1c03cb5d38
seonje <leeseonje9323@gmail.com>
2022-11-11 23:35:28 +0900

    Initial commit

---
+++ src/main/java/toy/game/idle/IdleApplication.java
@@ -0,0 +1,13 @@
+package toy.game.idle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toy/game/idle/IdleApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.game.idle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
