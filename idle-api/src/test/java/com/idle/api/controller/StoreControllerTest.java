2bece22f1ebf633d5cf558090740cc6921f09acf
seonje <leeseonje9323@gmail.com>
2022-12-15 00:01:35 +0900

    store api controller reseponse test

--- idle-api/src/test/java/com/idle/api/controller/StoreControllerTest.java
+++ idle-api/src/test/java/com/idle/api/controller/StoreControllerTest.java
@@ -1,9 +1,52 @@
 package com.idle.api.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.idle.api.controller.dto.StorePurchaseDto;
+import com.idle.api.service.StoreApiService;
+import com.idle.api.service.dto.StoreItemDto;
+import com.idle.shop.domain.weapon.Product;
+import com.idle.weapon.domain.Grade;
+import com.idle.weapon.domain.Name;
+import com.idle.weapon.domain.Weapon;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.*;
+import static org.springframework.http.MediaType.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(StoreController.class)
 class StoreControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    StoreApiService storeApiService;
 
 
+    @Test
+    void weapon_purchase_response() throws Exception {
+        StorePurchaseDto request = new StorePurchaseDto(1L, Product.RANDOM_WEAPON_BOX);
+        String requestBody = objectMapper.writeValueAsString(request);
+        StoreItemDto response = new StoreItemDto(2L, Weapon.of(Name.SWORD, Grade.NORMAL));
+        String responseBody = objectMapper.writeValueAsString(response);
+
+        when(storeApiService.weaponPurchase(request)).thenReturn(response);
+
+        this.mockMvc.perform(post("/store/weapon")
+                        .content(requestBody)
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseBody))
+                .andDo(print());
+    }
 }
