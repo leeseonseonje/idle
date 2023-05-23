bb010eba5bf44f1dcd16592ab37e96c4c2678836
seonje <leeseonje9323@gmail.com>
2023-05-24 00:31:56 +0900

    dockerfile 작성

--- idle-api/src/main/java/com/idle/IdleApplication.java
+++ idle-api/src/main/java/com/idle/IdleApplication.java
@@ -1,10 +1,7 @@
 package com.idle;
 
-import com.idle.member.repository.MemberRepository;
-import com.idle.money.repository.MoneyRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IdleApplication {
